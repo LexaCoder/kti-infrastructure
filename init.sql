e8d6b8c230a10e492046f93ce2208e3bc88f4dd6
kottik.mypp <kostiktakaya@gmail.com>
2022-10-21 19:52:12 +0300

    Changed init.sql

--- init.sql
+++ init.sql
@@ -1,3 +1,9 @@
+CREATE DATABASE flask;
+CREATE USER flask WITH ENCRYPTED PASSWORD 'Passw0rd!';
+GRANT ALL PRIVILEGES ON DATABASE flask TO flask;
+
+\c flask
+
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 	id serial primary key,
