5b139ba46797f5690a6d511f333724dc369efb24
kottik.mypp <kostiktakaya@gmail.com>
2022-10-21 20:40:38 +0300

    Changed init.sql

--- init.sql
+++ init.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE flask;
 CREATE USER flask WITH ENCRYPTED PASSWORD 'Passw0rd!';
+GRANT ALL PRIVILEGES ON DATABASE flask TO flask;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO flask;
 
 \c flask
 
@@ -9,5 +11,3 @@ CREATE TABLE users (
 	username varchar,
 	password varchar
 );
-
-GRANT ALL PRIVILEGES ON DATABASE flask TO flask;
